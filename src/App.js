f686e69200ce633113cf716b25a14d0a31a9115d
Fabian Villasenor <exionite@gmail.com>
2022-03-17 11:25:51 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/navbar/Navbar';
+import Header from './Components/Header-photo/Header';
+import Eldenring from './Components/Eldenring/Eldenring';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar-component">
+        <Navbar />
+      </div>
+      <div className="header-component">
+        <Header />
+      </div>
+      <div className="elden-component">
+        <Eldenring />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Eldenring/Eldenring.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Eldenring.css'
+
+export default function Eldenring() {
+  return (
+    <div className='elden-container'>
+        <div className="eldenring-text"></div>
+    </div>
+  )
+}
