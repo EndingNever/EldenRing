39129d1134f10722e042f531a5a6d8e844f4bb2d
Fabian Villasenor <exionite@gmail.com>
2022-03-17 17:09:40 -0700

    Navbar

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -2,9 +2,47 @@ import React from 'react'
 import './Navbar.css'
 
 export default function Navbar() {
+  const photos = [
+    {
+      url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoGames.svg'
+    },
+    {
+      url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoStore.svg'
+    },
+    {
+      url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoNews.svg'
+    },
+    {
+      url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoEpic.svg'
+    },
+    {
+      url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoSupport.svg'
+    },
+  ]
+  
+  console.log(photos[0].url)
+  
   return (
+    
     <div className='navbar-container'>
-        Navbar
+      <div className="first-two-container">
+        <div className="brand-logo">
+          {/* Bandai */}
+        </div>
+        <div className="navigation-bar">
+          <img src={photos[0].url} id='games' className='navigation-item'></img>
+          <img src={photos[1].url} id='store' className='navigation-item'></img>
+          <img src={photos[2].url} id='news' className='navigation-item'></img>
+          <img src={photos[3].url} id='ep!c' className='navigation-item'></img>
+          <img src={photos[4].url} id='support' className='navigation-item'></img>
+        </div>
+      </div>
+      <div className="search-container">
+          <input className='search-container-item' type="text" placeholder='Search' />
+          <img src='/images/navigation/email-svgrepo-com.svg' className='search-container-item' id='mail'></img>
+          <span id="hr"></span>
+          <img src='/images/navigation/user-svgrepo-com.svg' className='search-container-item' id="user"></img>
+      </div>
     </div>
   )
 }
