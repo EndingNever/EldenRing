c87753520f4c194f4dedc11fab419c74a3a4e4ea
Fabian Villasenor <exionite@gmail.com>
2022-03-23 14:08:54 -0700

    Final

--- src/Components/ShopNow/Shopnow.js
+++ src/Components/ShopNow/Shopnow.js
@@ -117,7 +117,71 @@ export default function Shopnow() {
                     </div>
                 </div>
                 <div className="guides-button">
-                    <button>See All Guides</button>
+                    <button>SEE ALL GUIDES</button>
+                </div>
+                <div className="elegant-space-container">
+                    <div className="elegant-space">
+                        {/* Ornament Background */}
+                    </div>
+                </div>
+                <div className="tarnished-text-container">
+                    <div className="guides-title">
+                        <h1>Tarnished of the Lands Between</h1>
+                    </div>
+                    <div className="tarnished-text">
+                        <p>
+                            The Golden Order has been shattered. Throughout the Lands Between, Demigods holding shards of the Elden Ring squabble and make war over the ruins of a perfect realm, now abandoned by the golden guidance of the Greater Will.
+                        </p>
+                        <p>
+                            As the echoes of this conflict thunder in the distance, an outcast arrives. Once, their ancestors called the Lands Between home, but the blessed light of grace was lost to their tribe long ago and they were expelled from the kingdom. They are the Tarnished, and they have returned to claim the Elden Lordship promised to them by legend.
+                        </p>
+                        <p>
+                            This is the world of ELDEN RING. As a Tarnished, the Lands Between await your exploration. You will ride through the vast fields, gallop over rolling hills, and leap to the top of rocky crags on your ephemeral steed, revealing a world teeming with life and danger.
+                        </p>
+                        <p>
+                            In the grand fields where your journey begins, mythic creatures prowl the veldts, ineffable horrors lurk in the bogs and marshes, and all manner of soldiers and itinerant warriors are waiting for those who wander unawares. Shy creatures nibble on sweet grasses or scuttle through the underbrush.
+                        </p>
+                        <p>
+                            Those few inhabitants who are not mad or hostile linger near the broken remnants of cities left behind by the Shattering. They may have answers for you, if you help them. Above them all, ensconced in vast legacies bristling with traps, secrets, and guardians, the Demigods – warped Lords who began as members of a royal and noble family – rule their domains with the unyielding power granted by shards of the Elden Ring.
+                        </p>
+                    </div>
+                </div>
+                <div className="guide-screenshots">
+                    <div className="elegant-border">{/* img */}</div>
+                    <div className="screenshots-container">
+                        <div className="screenshots screenshot-1">
+                            {/* Image */}
+                        </div>
+                        <div className="screenshots screenshot-2">
+                            {/* Image */}
+                        </div>
+                        <div className="screenshots screenshot-3">
+                            {/* Image */}
+                        </div>
+                    </div>
+                    <div className="elegant-border">{/* img */}</div>
+                </div>
+                <div className="guide-text-under-screenshots">
+                    <p>
+                        The guidance of lost graces – should you find them - will put you on a road to re-take these lands from the Demigods through might and magic, but you need not follow their path.
+                    </p>
+                    <p>
+                        The choice is yours. Do you crave power, or do you seek understanding? Decide for yourself, then build your character as you see fit.
+                    </p>
+                    <p>
+                        Try dozens of skills and find one to best suit your style. Practice stealth to avoid danger or catch enemies unaware. Use the environment, the weather, and the time of day to gain an advantage. Learn the art of combat , where you must read your enemy’s intentions and a well-timed dodge or parry could be the difference between life and death. Ride your steed into battle against mounted mercenaries and cut them from their horses. Master arcane spells from the masters that still linger among the ruins of the war. Summon familiar spirits to even the odds against you or call on your fellow Tarnished to fight at your side and share the burden as you explore. Or, delve into the complex, bloody history of the Shattering and discover the lost secrets of the Demigods and their kin. All these paths are possible, and more.
+                    </p>
+                    <p>
+                        Ultimately, your journey will be defined by the strength of your own ambition. The greater your goals, the greater the challenge will be. Should you choose to claim the Lands Between as your birthright then yes, you must fight.
+                    </p>
+                    <h3>And yes, you may die.</h3>
+                    <h3>But you will return to fight again.</h3>
+                    <h3>For that is how a champion – or a Lord – is born.</h3>
+                </div>
+                <div className="elegant-space-container">
+                    <div className="elegant-space">
+                        {/* Ornament Background */}
+                    </div>
                 </div>
             </div>
         </div >
