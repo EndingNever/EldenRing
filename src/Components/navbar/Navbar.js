6231b32d33880e612c0d2b33a10705b1e4cd5fd2
Fabian Villasenor <exionite@gmail.com>
2022-06-22 14:29:16 -0700

    new navbar

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -2,44 +2,38 @@ import React from 'react'
 import './Navbar.css'
 
 export default function Navbar() {
-  const photos = [
-    {
-      url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoGames.svg'
-    },
-    {
-      url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoStore.svg'
-    },
-    {
-      url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoNews.svg'
-    },
-    {
-      url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoEpic.svg'
-    },
-    {
-      url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoSupport.svg'
-    },
-  ]
+  // const photos = [
+  //   {
+  //     url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoGames.svg'
+  //   },
+  //   {
+  //     url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoStore.svg'
+  //   },
+  //   {
+  //     url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoNews.svg'
+  //   },
+  //   {
+  //     url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoEpic.svg'
+  //   },
+  //   {
+  //     url: 'https://en.bandainamcoent.eu/themes/custom/bne_main/dist/images/logoSupport.svg'
+  //   },
+  // ]
 
 
   return (
 
     <div className='navbar-container'>
       <div className="first-two-container">
-        <div className="brand-logo">
-          {/* Bandai */}
+        <div className="bandai-brand">
+          {/* Bandai Logo here */}
         </div>
         <div className="navigation-bar">
-          <img src={photos[0].url} id='games' className='navigation-item'></img>
-          <img src={photos[1].url} id='store' className='navigation-item'></img>
-          <img src={photos[2].url} id='news' className='navigation-item'></img>
-          <img src={photos[3].url} id='ep!c' className='navigation-item'></img>
-          <img src={photos[4].url} id='support' className='navigation-item'></img>
+          {/* Navbar goes here */}
         </div>
       </div>
       <div className="search-container">
-        <input className='search-container-item' type="text" placeholder='Search' />
-        <img src={process.env.PUBLIC_URL + '/images/navigation/email-svgrepo-com.svg'} className='search-container-item' id='mail'></img>
-        <span id="hr"></span>
+        <img src={process.env.PUBLIC_URL + "/images/navigation/icons8-search.svg"} alt="" />
         <img src={process.env.PUBLIC_URL + '/images/navigation/user-svgrepo-com.svg'} className='search-container-item' id="user"></img>
       </div>
     </div>
